3866cf841aac1a9e285721d45dff2b725f7810c2
Nhan Tran Vinh <nhan.tranvinh@employmenthero.com>
2025-05-10 08:37:30 +0700

    init

---
+++ Persistence/Migrations/20250510011612_Initial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+            
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name" }, // Specify columns, including the Id for seed data
+                values: new object[,]
+                {
+                    { 1, "Laptop Pro 15 inch" },
+                    { 2, "Wireless Mouse ergonomic" },
+                    { 3, "Mechanical Keyboard RGB" },
+                    { 4, "4K Monitor 27 inch" },
+                    { 5, "USB-C Hub 7-in-1" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ Persistence/ProductDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class ProductDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+}
